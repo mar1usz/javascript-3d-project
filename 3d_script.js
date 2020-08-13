ddd11ad8bbcbdd165ed3d57015f04052f27ba2a1
Mariusz <mariopl@poczta.onet.eu>
2020-08-13 08:24:52 +0200

    fix formatting

--- 3d_script.js
+++ 3d_script.js
@@ -1,62 +1,54 @@
-let scene, camera, renderer, controls
+let scene, camera, renderer, controls;
 
 function init() {
-  scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000)
-  camera.position.set(-900,-200,-900)
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
+  camera.position.set(-900,-200,-900);
   
-  renderer = new THREE.WebGLRenderer({ antialias: true })
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
   
   window.addEventListener('resize', function() {
     let WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight
-    renderer.setSize(WIDTH, HEIGHT)
-    camera.aspect = WIDTH / HEIGHT
-    camera.updateProjectionMatrix()
+    HEIGHT = window.innerHeight;
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
   })
   
-  controls = new THREE.OrbitControls(camera, renderer.domElement)
-  controls.addEventListener('change', renderer)
-  controls.minDistance = 500
-  controls.maxDistance = 1500
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', renderer);
+  controls.minDistance = 500;
+  controls.maxDistance = 1500;
   
+  let materialArray = [];
+  let texture_ft = new THREE.TextureLoader().load('resources/arid2_ft.jpg');
+  let texture_bk = new THREE.TextureLoader().load('resources/arid2_bk.jpg');
+  let texture_up = new THREE.TextureLoader().load('resources/arid2_up.jpg');
+  let texture_dn = new THREE.TextureLoader().load('resources/arid2_dn.jpg');
+  let texture_rt = new THREE.TextureLoader().load('resources/arid2_rt.jpg');
+  let texture_lf = new THREE.TextureLoader().load('resources/arid2_lf.jpg');
   
-  const materialArray = []
-  const texture_ft = new THREE.TextureLoader().load('resources/arid2_ft.jpg')
-  const texture_bk = new THREE.TextureLoader().load('resources/arid2_bk.jpg')
-  const texture_up = new THREE.TextureLoader().load('resources/arid2_up.jpg')
-  const texture_dn = new THREE.TextureLoader().load('resources/arid2_dn.jpg')
-  const texture_rt = new THREE.TextureLoader().load('resources/arid2_rt.jpg')
-  const texture_lf = new THREE.TextureLoader().load('resources/arid2_lf.jpg')
-  
-  texture_ft.anisotropy = renderer.getMaxAnisotropy()
-  texture_bk.anisotropy = renderer.getMaxAnisotropy()
-  texture_up.anisotropy = renderer.getMaxAnisotropy()
-  texture_dn.anisotropy = renderer.getMaxAnisotropy()
-  texture_rt.anisotropy = renderer.getMaxAnisotropy()
-  texture_lf.anisotropy = renderer.getMaxAnisotropy()
-  
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_ft }))
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_bk }))
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_up }))
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_dn }))
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_rt }))
-  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_lf }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_ft }));
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_bk }));
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_up }));
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_dn }));
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_rt }));
+  materialArray.push(new THREE.MeshBasicMaterial({ map: texture_lf }));
   
   for(let i = 0; i < 6; i++)
-      materialArray[i].side = THREE.BackSide
-  let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000)
-  let skybox = new THREE.Mesh(skyboxGeo, materialArray)
-  scene.add(skybox)
+      materialArray[i].side = THREE.BackSide;
+  let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+  let skybox = new THREE.Mesh(skyboxGeo, materialArray);
+  scene.add(skybox);
   
-  animate()
+  animate();
 }
 
 function animate () {
-  renderer.render(scene, camera)
-  window.requestAnimationFrame(animate)
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
 }
 
-init()
+init();
